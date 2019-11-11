d393318e25d918016d3ab97b0f1698c96ce34914
mmmries <riesmmm@gmail.com>
2019-11-10 22:07:32 -0700

    add test

--- test/fuel_gauge_test.exs
+++ test/fuel_gauge_test.exs
@@ -1,9 +1,17 @@
 defmodule Scenic.FuelGaugeTest do
   use ExUnit.Case
-  doctest Scenic.FuelGauge
+  alias Scenic.FuelGauge
 
-  test "draws itself onto a graph" do
-    graph = Scenic.Graph.build() |> Scenic.FuelGauge.draw(%{fuel: 0.5})
-    assert %Scenic.Graph{} = graph
+  @state %{
+    data: %{fuel: 0.5},
+    opts: %{styles: %{font: :roboto, font_size: 24}}
+  }
+
+  test "init" do
+    {:ok, _, push: _} = FuelGauge.init(:test_name, styles: %{})
+  end
+
+  test "handle_info {:fuel, level}" do
+    assert {:noreply, state, push: _} = FuelGauge.handle_info({:fuel, 0.5}, @state)
   end
 end
